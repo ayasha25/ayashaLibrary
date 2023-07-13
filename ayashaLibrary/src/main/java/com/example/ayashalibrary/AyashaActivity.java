1fd3da969f480d7bbe430a85381fe8c4e89aaa42
Vaibhav <vaibhavg7005@gmail.com>
2023-07-14 00:54:55 +0530

    library publish

---
+++ ayashaLibrary/src/main/java/com/example/ayashalibrary/AyashaActivity.java
@@ -0,0 +1,10 @@
+package com.example.ayashalibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class AyashaActivity {
+    public static void s(Context c, String message) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
+}
